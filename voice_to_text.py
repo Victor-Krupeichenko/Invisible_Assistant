319e72442c863e08e484c157f65f53fa5b088144
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2024-07-15 11:22:39 +0300

    Класс распознания речи и конвертации ее в текст

---
+++ voice_to_text.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+
+
+class SpeechToTextConverter:
+    """
+    Класс распознания речи и конвертации ее в текст
+    """
+
+    def __init__(self, microphone_id=0, language='ru'):
+        """
+        Инициализация распознавателя речи
+        :param microphone_id: ID микрофона, из которого будем получать аудио
+        :param language: Язык на котором будет производиться распознание речи
+        """
+        self.microphone_id = microphone_id
+        self.language = language
+        self.r = sr.Recognizer()
+        self.mic = sr.Microphone(device_index=microphone_id)
+
+    def recognize_speech(self):
+        """
+        Распознаем голос и вернем текст, сгенерированный распознавателем речи
+        :return: текст, распознанный голосом
+        """
+        with self.mic as source:
+            self.r.adjust_for_ambient_noise(source, duration=0.5)
+            # print("Слушаю...")
+            audio = self.r.listen(source)
+            try:
+                return self.r.recognize_google(audio, language=self.language)
+            except sr.UnknownValueError as exc:
+                print(f'Распознание речи не удалось {exc}')
+            except sr.RequestError as exc:
+                print(f'Не удалось запросить результаты от службы распознавания речи Google. {exc}')
