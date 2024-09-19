6c2634197ab0e8692609e38321a2a90c899a01ab
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2024-09-19 10:23:08 +0300

    Класс распознания речи и конвертации ее в текст

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+from settings_env import mic_id, language
 
 
 class SpeechToTextConverter:
@@ -6,32 +7,39 @@ class SpeechToTextConverter:
     Класс распознания речи и конвертации ее в текст
     """
 
-    def __init__(self, microphone_id=0, language='ru'):
+    def __init__(self, microphone_id=mic_id, lang=language):
         """
         Инициализация распознавателя речи
         :param microphone_id: ID микрофона, из которого будем получать аудио
-        :param language: Язык на котором будет производиться распознание речи
+        :param lang: Язык на котором будет производиться распознание речи
         """
         self.microphone_id = microphone_id
-        self.language = language
+        self.language = lang
         self.r = sr.Recognizer()
         self.mic = sr.Microphone(device_index=microphone_id)
 
-    def recognize_speech(self):
+    def recognize_speech(self, timeout=None):
         """
         Распознаем голос и вернем текст, сгенерированный распознавателем речи
-        :return: текст, распознанный голосом
+        :param timeout: Время ожидания начало фразы, по истечении которого распознавание прекращается, в секундах
+        :return: текст, распознанный голосом или None
         """
         with self.mic as source:
-            self.r.energy_threshold = 15000
-            self.r.adjust_for_ambient_noise(source, duration=.5)
-            print("Слушаю...")
-            audio = self.r.listen(source)
+            self.r.energy_threshold = 16000
+            self.r.adjust_for_ambient_noise(source, 1.2)
+            print("Слушаю...")  # Выводим сообщение о начале слушания
             try:
+                audio = self.r.listen(source, timeout=timeout)
                 text_recognition = self.r.recognize_google(audio, language=self.language)
                 return text_recognition
-            except sr.UnknownValueError as exc:
-                print(f'Распознание речи не удалось {exc}')
-            except sr.RequestError as exc:
-                print(f'Не удалось запросить результаты от службы распознавания речи Google. {exc}')
+            except sr.UnknownValueError:
+                # TODO придумать что делать с исключениями в место pass
+                pass
+                # print(f'Распознание речи не удалось {exc}')
+            except sr.RequestError:
+                pass
+                # print(f'Не удалось запросить результаты от службы распознавания речи Google. {exc}')
+            except sr.WaitTimeoutError:
+                pass
+                # print(f'Превышено время ожидания. {exc}')
             return None
