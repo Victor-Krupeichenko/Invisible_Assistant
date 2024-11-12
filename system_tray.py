3ddd47851e57f7399f3d470e0adc9d9d5346451a
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2024-11-12 21:44:01 +0300

    Класс для работы с иконкой в трее

--- system_tray.py
+++ system_tray.py
@@ -1,6 +1,6 @@
 import pystray
 import PIL.Image
-from settings_env import icon_tray
+from settings_env import icon_tray, exit_program
 
 
 class IconTray:
@@ -17,14 +17,16 @@ class IconTray:
         self.starter = starter
         self.image = PIL.Image.open(icon)
 
-    def click_on_icon(self, icon, item):
+    def click_on_icon(self, close_process, item):
         """
         Обработчик клика по иконке в трее
+        :param close_process: процесс, который нужно будет закрыть
+        :param item: раздел 'выход' появляющегося меню над иконкой
         """
-        if str(item) == 'Выход':
+        if str(item).lower() == exit_program:
             # Остановка основного процесса (предположим, icon.stop() это остановка основного процесса)
             self.starter.stop_program()
-            icon.stop()
+            close_process.stop()
 
     def create_icon_tray(self):
         """
@@ -34,6 +36,6 @@ class IconTray:
             name='invisible_assistant',
             icon=self.image,
             title='Invisible Assistant',
-            menu=pystray.Menu(pystray.MenuItem(text='Выход', action=self.click_on_icon))
+            menu=pystray.Menu(pystray.MenuItem(text=exit_program.upper(), action=self.click_on_icon))
         )
         create_icon.run()
